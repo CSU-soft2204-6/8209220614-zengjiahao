af9402a4e5997ee5291853b7dd2fa94b76801b5b
icel <119909700+The-Augenstern@users.noreply.github.com>
2023-01-07 23:33:49 +0800

    Update 第一题.cpp

--- 实验1-cpp/第一题.cpp
+++ 实验1-cpp/第一题.cpp
@@ -1,11 +1,11 @@
-//#include<iostream>
-//using namespace std;
-//int main() {
-//	int k = 5;
-//	int i = k + 1;
-//	cout << i++ << endl;
-//	int a = 1;
-//	cout << a++ << endl;
-//	cout << "Welcome to c++!" << endl;
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+int main() {
+	int k = 5;
+  int i = k + 1;
+	cout << i++ << endl;
+	int a = 1;
+	cout << a++ << endl;
+	cout << "Welcome to c++!" << endl;
+	return 0;
+}
